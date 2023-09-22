d49ff45691bc0ae1cc93d23ec4fec86a33e99a24
LTQX <142605634+LTQX@users.noreply.github.com>
2023-09-22 16:21:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CockClock;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Time:" + DateTime.Now.ToString("HH:mm:ss:"));
+            Thread.Sleep(1000);
+            Console.Write(new string(' ', Console.WindowWidth));
+        }
+    }
+}
